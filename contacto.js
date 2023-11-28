1e8a2f897eae02e1f5c4448853b0df8fe64bc74b
juanpablodcouto <juanpablodemontecouto@hotmail.com>
2023-11-28 15:47:33 -0300

    Segunda pre entrega Javascript

---
+++ contacto.js
@@ -0,0 +1,15 @@
+/*FUNCIÓN: SENCILLAMENTE SE LE DÁ LA BIENVENIDA A LA SECCIÓN MEDIANTE LA CUAL EL VISITANTE PUEDE CONTACTARSE CON NOSOTROS  SE LE DA LA CERTEZ QUE LA COMUNICACIÓN ESTA HABLITADA Y QUE A LA BREVEDAD DEVOLVEREMOS EL MENSAJE O NOS PONDREMOS EN CONTACTO*/
+
+
+"use strict";
+
+function contacto (mensaje) {
+    alert (mensaje);
+}
+
+contacto("En esta sección podrás contactarte con nosotros. Dejanos tu mensaje y a la brevedad un agente de nuestra página se contactará contigo!");
+
+
+
+
+
